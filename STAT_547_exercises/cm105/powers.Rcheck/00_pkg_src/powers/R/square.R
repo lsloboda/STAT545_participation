5d970819559401f7e6ea48a8f487bbe4f908896f
Laura Sloboda <laura.sloboda@gmail.com>
2018-11-08 10:35:55 -0800

    interim cm105, 106

---
+++ STAT_547_exercises/cm105/powers.Rcheck/00_pkg_src/powers/R/square.R
@@ -0,0 +1,14 @@
+#' @title Square the input
+#' @param x Vector of numerics
+#' @return The vector x, squared
+#' @export
+
+square <- function(x) x^2
+
+#in console, run:
+#install.packages("roxygen2")
+#library(devtools)
+#document()
+#then manually delete the NAMESPACE file from the root folder -> note that now that we added export, this is auto generated!
+
+

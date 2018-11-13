b507600c592a6619956d35895422a075927e6e46
Laura Sloboda <laura.sloboda@gmail.com>
2018-11-13 10:06:28 -0800

    interim

--- STAT_547_exercises/cm107/bcl/app.R
+++ STAT_547_exercises/cm107/bcl/app.R
@@ -9,42 +9,62 @@
 
 library(shiny)
 
+#the skeleton required in all Shiny apps: ui, server, run the application (this will give a blank page)
+
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-   
-   # Application title
-   titlePanel("Old Faithful Geyser Data"),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
-      ),
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("distPlot")
-      )
-   )
+   "This is some text.",
+   "This is some more text.",
+    tags$h1("Level 1 Header"), #using the tags list is one way to access tags, but not the only way, as shown below
+    h1(em("Level 1 Header, Part 2")), #can nest tags, as shown here
+    HTML("<h1>Level 1 Header, Part 3</h1>"),
+    headerPanel("Yes!"),
+    a(href="https://google.ca", "Link to Google!"),
+    tags$b("This is bold."),
+    tags$caption("This is a caption")
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
    
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
-   })
+
 }
 
 # Run the application 
 shinyApp(ui = ui, server = server)
 
+
+
+
+
+
+
+
+
+
+# # Application title
+# titlePanel("Old Faithful Geyser Data"),
+# 
+# # Sidebar with a slider input for number of bins 
+# sidebarLayout(
+#   sidebarPanel(
+#     sliderInput("bins",
+#                 "Number of bins:",
+#                 min = 1,
+#                 max = 50,
+#                 value = 30)
+#   ),
+#   
+#   # Show a plot of the generated distribution
+#   mainPanel(
+#     plotOutput("distPlot")
+#   )
+# )
+# output$distPlot <- renderPlot({
+#   # generate bins based on input$bins from ui.R
+#   x    <- faithful[, 2] 
+#   bins <- seq(min(x), max(x), length.out = input$bins + 1)
+#   
+#   # draw the histogram with the specified number of bins
+#   hist(x, breaks = bins, col = 'darkgray', border = 'white')
+# })

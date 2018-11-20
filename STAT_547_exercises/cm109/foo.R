08a9d40d8d3a5901b3cbb0cd5587dc8e12548e97
Laura Sloboda <laura.sloboda@gmail.com>
2018-11-20 09:57:15 -0800

    interim

--- STAT_547_exercises/cm109/foo.R
+++ STAT_547_exercises/cm109/foo.R
@@ -1,3 +1,8 @@
 library(ggplot2)
 qplot(1:10, 1:10)
 ggsave("plot.png")
+
+#clicking source will do effectively the same as knitting (runs the script from top to bottom)
+
+#if we go in new terminal and navigate to the directory of the file, can put the command Rscript and run the file and functions within the file
+#good practice to keep the input unchanged by the functions (will make things less confusing)
